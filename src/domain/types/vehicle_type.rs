16abc39af90a12e201dbe1303b1f63ccc561e72d
Ion Dodon <iondodon2@gmail.com>
2023-08-09 22:42:29 +0300

    Added the Add new vehicle endpoint

--- src/domain/types/vehicle_type.rs
+++ src/domain/types/vehicle_type.rs
@@ -2,6 +2,7 @@ use rocket::serde::{Serialize, Deserialize};
 
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(rename_all = "UPPERCASE")]
 pub enum VehicleType {
     SmallCar,
     EstateCar,
